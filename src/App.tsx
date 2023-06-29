5f7f0b9e5f416046c220858d1f7d9b112a57d5a1
Gunjan Limbachiya <gunjan.limbachiya@publicissapient.com>
2023-06-29 13:51:43 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PasswordLengthSelector from "./components/PasswordLengthSelector";
 
 function App() {
+  const [length, setLength] = useState<number>(4);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="container">
+        <h1>Password Generator</h1>
+        <div className="box">
+          <div>GHUfgy78#$</div> <button>Copy</button>
+        </div>
+        <div>
+          <label htmlFor="password-length">
+            Length: <span className="value">4</span>
+          </label>
+          <PasswordLengthSelector />
+        </div>
+
+        <div>
+          <label htmlFor="password-length">Settings:</label>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/PasswordLengthSelector.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PasswordLengthSelector = () => {
+  return (
+    <>
+      <div className="box">
+        <span>4</span>{" "}
+        <input
+          type="range"
+          min="4"
+          max="20"
+          name="length"
+          style={{ flexGrow: "1", margin: "0px .5rem " }}
+          id="password-length"
+        />
+        <span>20</span>
+      </div>
+    </>
+  );
+};
+
+export default PasswordLengthSelector;
