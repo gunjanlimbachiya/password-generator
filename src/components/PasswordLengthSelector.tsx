20203dc5e1a76cbbd816a335d302c753a8690555
Gunjan Limbachiya <67776947+gunjanlimbachiya@users.noreply.github.com>
2023-06-29 14:38:28 +0530

    Update PasswordLengthSelector.tsx

--- src/components/PasswordLengthSelector.tsx
+++ src/components/PasswordLengthSelector.tsx
@@ -8,10 +8,10 @@ const PasswordLengthSelector = () => {
         <input
           type="range"
           min="4"
-          max="205"
+          max="20"
           name="length"
           style={{ flexGrow: "1", margin: "0px .5rem " }}
-          id="password-length2"
+          id="password-length"
         />
         <span>20</span>
       </div>
